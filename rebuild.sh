f0da493be4ca754fd7b7f2c7ba090104fa990d2e
Sahil Dinanath <sahildinanath@gmail.com>
2024-04-22 00:15:45 +0200

    44 current  2024-04-22 00:11:31  24.05.20240416.5672bc9   6.6.27                          *

--- rebuild.sh
+++ rebuild.sh
@@ -2,11 +2,9 @@
 set -e
 pushd ~/.dotfiles/
 alejandra . &>/dev/null
- git diff -U0 *.nix
- echo "NixOS Rebuilding..."
- sudo nixos-rebuild switch --flake .#default &> nixos-switch.log || (
-  cat nixos-switch.log | grep --color error && false)
- gen=$(nixos-rebuild list-generations | grep current)
- git commit -am "$gen"
- git push origin main
+git diff -U0 *.nix
+echo "NixOS Rebuilding..."
+sudo nixos-rebuild switch --flake .#default
+gen=$(nixos-rebuild list-generations | grep current)
+git commit -am "$gen"
 popd
