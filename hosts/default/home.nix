b24a121b99b0ad2ef49a20b7c0341557746b1e4c
Sahil Dinanath <sahildinanath@gmail.com>
2024-04-21 23:33:18 +0200

    first iteration

---
+++ hosts/default/home.nix
@@ -0,0 +1,162 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "sahil";
+  home.homeDirectory = "/home/sahil";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+    pkgs.tmux
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/sahil/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+     EDITOR = "nvim";
+  };
+  
+  programs.git = {
+  enable = true;
+  userName = "Sahil Dinanath";
+  userEmail = "sahildinanath@gmail.com";
+  extraConfig = {
+  init.defaultBranch = "main";
+  safe.directory = "/etc/nixos";
+  };
+  };
+  
+  programs.neovim = {
+	enable = true;
+	viAlias = true;
+	vimAlias = true;
+	
+  };
+
+  programs.bash = {
+	enable = true;
+	initExtra = "
+	# set tmux to open on start of interactive shell
+	# code below checks whether tmux exists on the system
+	# we're in an interactive shell
+	# that tmux doesn't try to run within itself
+	if [ -n \"$PS1\" ] && [ -z \"$TMUX\" ]; then
+ 	 	# Adapted from https://unix.stackexchange.com/a/176885/347104
+  		# Create session 'main' or attach to 'main' if already exists.
+  		tmux new-session -A -s main
+	fi";
+};
+
+programs.tmux = {
+  enable = true;
+  historyLimit = 100000;
+  baseIndex = 1;
+  escapeTime = 0;
+  keyMode = "vi";
+  mouse = true;
+  prefix = "C-Space";
+# makes tmux not mess up colours 
+  terminal = "tmux-256color";
+
+
+  plugins = with pkgs;[
+       tmuxPlugins.sensible
+      tmuxPlugins.vim-tmux-navigator
+       {
+       plugin = tmuxPlugins.resurrect;
+       extraConfig = "set -g @resurrect-capture-pane-content 'on'";
+       }
+  
+       {
+       plugin = tmuxPlugins.continuum;
+       extraConfig = "set -g @continuum-restore 'on'";
+       }
+  
+       ];
+  extraConfig = ''
+# options
+
+#add whatever this does
+set-option -g focus-events on
+
+#set clipboard 
+set -s set-clipboard external
+
+# Keymaps
+# alt+H/L to move between windows
+bind -n M-h previous-window
+bind -n M-l next-window
+
+#changes copying in tmux buffers
+#bind-key -T copy-mode-vi v send-keys -X begin-selection
+#bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+#bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+#open panes in current directory
+bind '"' split-window -v -c "#{pane_current_path}"
+bind % split-window -h -c "#{pane_current_path}"
+bind c new-window -c "#{pane_current_path}"
+
+#fullscreen pane to be the same as window manager
+bind -r m resize-pane -Z
+  '';
+};
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
