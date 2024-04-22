4325221f8a99b4b9ac7a5ebf1af1d27adc8cce8d
Sahil Dinanath <sahildinanath@gmail.com>
2024-04-22 23:22:06 +0200

    49 current  2024-04-22 23:22:00  24.05.20240416.5672bc9   6.6.27                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -88,6 +88,34 @@
     enable = true;
     viAlias = true;
     vimAlias = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+    #
+    #    plugins = with pkgs.vimPlugins; [
+    #       vim-tmux-navigator
+    #        lazygit-nvim
+    # vim-sleuth
+    #
+    # telescope-nvim
+    # telescope-fzf-native-nvim
+    #
+    # nvim-cmp
+    # nvim-lspconfig
+    #
+    # neodev-nvim
+    #
+    #
+    #
+    #
+    #
+    #
+    #    ];
+    #
+    #    extraPackages = with pkgs; [
+    #      wl-clipboard
+    #      xclip
+    #      lazygit
+    #    ];
   };
 
   programs.fish = {
