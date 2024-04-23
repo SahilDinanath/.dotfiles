7a0c42c4454f00cc702d17c769a5fc7853d7656c
Sahil Dinanath <sahildinanath@gmail.com>
2024-04-23 23:34:34 +0200

    58 current  2024-04-23 23:31:14  24.05.20240416.5672bc9   6.6.27                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -143,63 +143,9 @@
 	fi";
   };
 
-  programs.tmux = {
-    enable = true;
-    historyLimit = 100000;
-    baseIndex = 1;
-    escapeTime = 0;
-    keyMode = "vi";
-
-    mouse = true;
-    prefix = "C-Space";
-    # makes tmux not mess up colours
-    terminal = "tmux-256color";
-    shell = "${pkgs.fish}/bin/fish";
-
-    plugins = with pkgs; [
-      tmuxPlugins.sensible
-      tmuxPlugins.vim-tmux-navigator
-      {
-        plugin = tmuxPlugins.resurrect;
-        extraConfig = "set -g @resurrect-capture-pane-content 'on'";
-      }
-
-      {
-        plugin = tmuxPlugins.continuum;
-        extraConfig = "set -g @continuum-restore 'on'";
-      }
-    ];
-    extraConfig = ''
-      #add whatever this does
-      set-option -g focus-events on
-
-      #set clipboard
-      set -s set-clipboard external
-
-      # Keymaps
-      # alt+H/L to move between windows
-      bind -n M-h previous-window
-      bind -n M-l next-window
-
-      #changes copying in tmux buffers
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
-      #open panes in current directory
-      bind '"' split-window -v -c "#{pane_current_path}"
-      bind % split-window -h -c "#{pane_current_path}"
-      bind c new-window -c "#{pane_current_path}"
-
-      #fullscreen pane to be the same as window manager
-      bind -r m resize-pane -Z
-
-
-    '';
-  };
-
   imports = [
     ../../packages/git/git.nix
+    ../../packages/tmux/tmux.nix
   ];
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
