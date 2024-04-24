a33deb46b0b0f8c4ed7c774e87f82191d0ea5bd2
Sahil Dinanath <sahildinanath@gmail.com>
2024-04-24 23:21:46 +0200

    63 current  2024-04-24 23:21:38  24.05.20240421.6143fc5   6.6.28                          on-the-go

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -113,9 +113,31 @@
 
   hardware.nvidia.prime = {
     sync.enable = true;
+
     # Make sure to use the correct Bus ID values for your system!
     amdgpuBusId = "PCI:52:0:0";
     nvidiaBusId = "PCI:1:0:0";
+  };
+  specialisation = {
+    on-the-go.configuration = {
+      system.nixos.tags = ["on-the-go"];
+      boot.extraModprobeConfig = ''
+        blacklist nouveau
+        options nouveau modeset=0
+      '';
+
+      services.udev.extraRules = ''
+        # Remove NVIDIA USB xHCI Host Controller devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA USB Type-C UCSI devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA Audio devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA VGA/3D controller devices
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+      '';
+      boot.blacklistedKernelModules = ["nouveau" "nvidia" "nvidia_drm" "nvidia_modeset"];
+    };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
