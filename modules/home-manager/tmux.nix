490d96b12c3890cf6b0a10e36a06d1259638cc66
Sahil Dinanath <sahildinanath@gmail.com>
2024-04-28 21:55:32 +0200

    112 current  2024-04-28 21:55:22  24.05.20240421.6143fc5   6.6.28                          on-the-go

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -30,29 +30,34 @@
       }
     ];
     extraConfig = ''
-      #add whatever this does
-            set-option -g focus-events on
+            #add whatever this does
+                  set-option -g focus-events on
 
-            #set clipboard
-            set -s set-clipboard external
+                  #set clipboard
+                  set -s set-clipboard external
 
-            # Keymaps
-            # alt+H/L to move between windows
-            bind -n M-h previous-window
-            bind -n M-l next-window
+                  # Keymaps
+                  # alt+H/L to move between windows
+                  bind -n M-h previous-window
+                  bind -n M-l next-window
 
-            #changes copying in tmux buffers
-            bind-key -T copy-mode-vi v send-keys -X begin-selection
-            bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-            bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+                  #changes copying in tmux buffers
+                  bind-key -T copy-mode-vi v send-keys -X begin-selection
+                  bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+                  bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
 
-            #open panes in current directory
-            bind '"' split-window -v -c "#{pane_current_path}"
-            bind % split-window -h -c "#{pane_current_path}"
-            bind c new-window -c "#{pane_current_path}"
+                  #open panes in current directory
+                  bind '"' split-window -v -c "#{pane_current_path}"
+                  bind % split-window -h -c "#{pane_current_path}"
+                  bind c new-window -c "#{pane_current_path}"
 
-            #fullscreen pane to be the same as window manager
-            bind -r m resize-pane -Z
+                  #fullscreen pane to be the same as window manager
+                  bind -r m resize-pane -Z
+                  #resize window
+                  bind -r j resize-pane -D 5
+                  bind -r k resize-pane -U 5
+      bind -r l resize-pane -R 5
+      bind -r h resize-pane -L 5
     '';
   };
 }
