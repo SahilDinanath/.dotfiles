459423920227480e72cb985700cf15ced96f5314
Sahil Dinanath <sahildinanath@gmail.com>
2024-04-28 20:40:35 +0200

    106 current  2024-04-28 20:40:25  24.05.20240421.6143fc5   6.6.28                          on-the-go

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -225,9 +225,12 @@
         i3status # gives you the default i3 status bar
         i3lock #default i3 screen locker
         i3blocks #if you are planning on using i3blocks over i3status
+        polybar
         feh
         flameshot
         pulseaudio
+        brightnessctl
+        networkmanagerapplet
       ];
     };
   };
