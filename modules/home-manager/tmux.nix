831894b52ddc04186f07d0da7508b44471bf7aed
Sahil Dinanath <sahildinanath@gmail.com>
2024-04-28 21:00:35 +0200

    109 current  2024-04-28 21:00:26  24.05.20240421.6143fc5   6.6.28                          on-the-go

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -28,32 +28,5 @@
         extraConfig = "set -g @continuum-restore 'on'";
       }
     ];
-    extraConfig = ''
-      #add whatever this does
-      set-option -g focus-events on
-
-      #set clipboard
-      set -s set-clipboard external
-
-      # Keymaps
-      # alt+H/L to move between windows
-      bind -n M-h previous-window
-      bind -n M-l next-window
-
-      #changes copying in tmux buffers
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
-      #open panes in current directory
-      bind '"' split-window -v -c "#{pane_current_path}"
-      bind % split-window -h -c "#{pane_current_path}"
-      bind c new-window -c "#{pane_current_path}"
-
-      #fullscreen pane to be the same as window manager
-      bind -r m resize-pane -Z
-
-
-    '';
   };
 }
