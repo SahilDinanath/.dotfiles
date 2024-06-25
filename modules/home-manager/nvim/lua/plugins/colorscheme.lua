24c910064119f77f645666214f61b7da5d75e284
Sahil Dinanath <sahildinanath@gmail.com>
2024-06-25 11:44:37 +0200

    263 current  2024-06-25 11:44:31  24.11.20240620.d603719  6.9.5                           no-nvidia

--- modules/home-manager/nvim/lua/plugins/colorscheme.lua
+++ modules/home-manager/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,8 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+	"sainnhe/gruvbox-material",
 	priority = 1000,
 	config = function()
-		require("gruvbox").setup({
-			contrast = "hard",
-		})
+		require("gruvbox").setup({})
 		vim.cmd("colorscheme gruvbox")
 	end,
 }
