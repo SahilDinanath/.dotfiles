c55763ef0f21fff834dd4945ae20bac81a1ddbb9
Sahil Dinanath <sahildinanath@gmail.com>
2024-06-25 11:54:53 +0200

    264 current  2024-06-25 11:54:46  24.11.20240620.d603719  6.9.5                           no-nvidia

--- modules/home-manager/nvim/lua/plugins/colorscheme.lua
+++ modules/home-manager/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
 	"sainnhe/gruvbox-material",
+	lazy = false,
 	priority = 1000,
 	config = function()
-		require("gruvbox").setup({})
-		vim.cmd("colorscheme gruvbox")
+		-- Optionally configure and load the colorscheme
+		-- directly inside the plugin declaration.
+		vim.g.gruvbox_material_background = "hard"
+		vim.cmd.colorscheme("gruvbox-material")
 	end,
 }
