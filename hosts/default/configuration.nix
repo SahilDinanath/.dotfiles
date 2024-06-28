17bd0665b9f18a841f7220e83ea0d77a31da096a
Sahil Dinanath <sahildinanath@gmail.com>
2024-06-28 15:26:27 +0200

    270 current  2024-06-28 13:46:01  24.11.20240624.2893f56  6.9.6                           no-nvidia

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -70,7 +70,7 @@
   };
 
   #add Nvidia support if not in specialisation aka no-nvidia
-  config = lib.mkIf (config.specialisation == { }) {
+  lib.optionalAttrs (config.specialisation == { }) {
     #Nvidia GPU support
     # Enable OpenGL
     hardware.graphics.enable = true;
