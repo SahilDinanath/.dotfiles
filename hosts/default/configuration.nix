c891adc40e03db51d6f5a60217afe7167828dda1
Sahil Dinanath <sahildinanath@gmail.com>
2024-06-29 12:53:44 +0200

    273 current  2024-06-29 12:37:30  24.11.20240624.2893f56  6.9.6                           no-nvidia

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -73,57 +73,59 @@
   #Nvidia GPU support
   # Enable OpenGL
 
-  hardware.graphics.enable = true;
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
+  config = lib.mkIf (config.specialisation != { }) {
+    hardware.graphics.enable = true;
+    # Load nvidia driver for Xorg and Wayland
+    services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use. Experimental.
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  hardware.nvidia.prime = {
-    sync.enable = true;
-
-    amdgpuBusId = "PCI:52:0:0";
-    nvidiaBusId = "PCI:1:0:0";
-  };
-  nixpkgs.config.cudaSupport = true;
-
-  specialisation = {
-    no-nvidia.configuration = {
-      system.nixos.tags = [ "no-nvidia" ];
-      boot.extraModprobeConfig = ''
-        blacklist nouveau
-        options nouveau modeset=0
-      '';
-
-      services.udev.extraRules = ''
-        # Remove NVIDIA USB xHCI Host Controller devices, if present
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-        # Remove NVIDIA USB Type-C UCSI devices, if present
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-        # Remove NVIDIA Audio devices, if present
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-        # Remove NVIDIA VGA/3D controller devices
-        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-      '';
-      boot.blacklistedKernelModules = [
-        "nouveau"
-        "nvidia"
-        "nvidia_drm"
-        "nvidia_modeset"
-      ];
+    hardware.nvidia = {
+      # Modesetting is required.
+      modesetting.enable = true;
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      powerManagement.enable = false;
+      # Fine-grained power management. Turns off GPU when not in use. Experimental.
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
-  };
 
+    hardware.nvidia.prime = {
+      sync.enable = true;
+
+      amdgpuBusId = "PCI:52:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+    nixpkgs.config.cudaSupport = true;
+
+    specialisation = {
+      no-nvidia.configuration = {
+        system.nixos.tags = [ "no-nvidia" ];
+        boot.extraModprobeConfig = ''
+          blacklist nouveau
+          options nouveau modeset=0
+        '';
+
+        services.udev.extraRules = ''
+          # Remove NVIDIA USB xHCI Host Controller devices, if present
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+          # Remove NVIDIA USB Type-C UCSI devices, if present
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+          # Remove NVIDIA Audio devices, if present
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+          # Remove NVIDIA VGA/3D controller devices
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+        '';
+        boot.blacklistedKernelModules = [
+          "nouveau"
+          "nvidia"
+          "nvidia_drm"
+          "nvidia_modeset"
+        ];
+      };
+    };
+
+  };
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
