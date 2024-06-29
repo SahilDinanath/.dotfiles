f4251451bc0ce0fce8068acacce993b9f4d6b2c7
Sahil Dinanath <sahildinanath@gmail.com>
2024-06-29 12:55:09 +0200

    273 current  2024-06-29 12:37:30  24.11.20240624.2893f56  6.9.6                           no-nvidia

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -72,6 +72,32 @@
   #add Nvidia support if not in specialisation aka no-nvidia
   #Nvidia GPU support
   # Enable OpenGL
+  specialisation = {
+    no-nvidia.configuration = {
+      system.nixos.tags = [ "no-nvidia" ];
+      boot.extraModprobeConfig = ''
+        blacklist nouveau
+        options nouveau modeset=0
+      '';
+
+      services.udev.extraRules = ''
+        # Remove NVIDIA USB xHCI Host Controller devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA USB Type-C UCSI devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA Audio devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA VGA/3D controller devices
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+      '';
+      boot.blacklistedKernelModules = [
+        "nouveau"
+        "nvidia"
+        "nvidia_drm"
+        "nvidia_modeset"
+      ];
+    };
+  };
 
   config = lib.mkIf (config.specialisation != { }) {
     hardware.graphics.enable = true;
@@ -97,34 +123,6 @@
       nvidiaBusId = "PCI:1:0:0";
     };
     nixpkgs.config.cudaSupport = true;
-
-    specialisation = {
-      no-nvidia.configuration = {
-        system.nixos.tags = [ "no-nvidia" ];
-        boot.extraModprobeConfig = ''
-          blacklist nouveau
-          options nouveau modeset=0
-        '';
-
-        services.udev.extraRules = ''
-          # Remove NVIDIA USB xHCI Host Controller devices, if present
-          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-          # Remove NVIDIA USB Type-C UCSI devices, if present
-          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-          # Remove NVIDIA Audio devices, if present
-          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-          # Remove NVIDIA VGA/3D controller devices
-          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-        '';
-        boot.blacklistedKernelModules = [
-          "nouveau"
-          "nvidia"
-          "nvidia_drm"
-          "nvidia_modeset"
-        ];
-      };
-    };
-
   };
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
