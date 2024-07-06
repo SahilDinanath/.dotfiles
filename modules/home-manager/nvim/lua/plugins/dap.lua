131c064fa43407d3b301bdbd93559ee409ba21d4
Sahil Dinanath <sahildinanath@gmail.com>
2024-07-06 20:46:14 +0200

    309 current  2024-07-06 20:46:04  24.11.20240703.9f4128e  6.9.7                           no-nvidia

--- modules/home-manager/nvim/lua/plugins/dap.lua
+++ modules/home-manager/nvim/lua/plugins/dap.lua
@@ -1,9 +1,10 @@
 return {
 	"rcarriga/nvim-dap-ui",
-	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio", "theHamsta/nvim-dap-virtual-text" },
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
+		require("nvim-dap-virtual-text").setup()
 		dapui.setup()
 		vim.keymap.set("n", "<F5>", function()
 			dap.continue()
