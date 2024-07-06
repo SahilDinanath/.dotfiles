134aa8befcb25153bd4de640d5536755b6de8cbd
Sahil Dinanath <sahildinanath@gmail.com>
2024-07-06 23:23:17 +0200

    313 current  2024-07-06 23:23:05  24.11.20240703.9f4128e  6.9.7                           no-nvidia

--- modules/home-manager/nvim/lua/plugins/undotree.lua
+++ modules/home-manager/nvim/lua/plugins/undotree.lua
@@ -2,6 +2,6 @@ return {
 	"mbbill/undotree",
 
 	config = function()
-		vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+		vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle, { desc = "[U]ndo tree [T]oggle" })
 	end,
 }
