abaa1101f782e876be44443a23770ba8af3c2103
Sahil Dinanath <sahildinanath@gmail.com>
2024-07-07 20:00:07 +0200

    335 current  2024-07-07 19:36:57  24.11.20240703.9f4128e  6.9.7                           no-nvidia

--- modules/home-manager/bash.nix
+++ modules/home-manager/bash.nix
@@ -41,10 +41,11 @@
       bind '"jk":vi-movement-mode'
 
     '';
-    interactiveShellInit = ''
-      set show-mode-in-prompt on
-      set vi-cmd-mode-string "\1\e[2 q\2"
-      set vi-ins-mode-string "\1\e[6 q\2"
-    '';
   };
+
+  environment.etc.".inputrc".text = ''
+    set show-mode-in-prompt on
+    set vi-cmd-mode-string "\1\e[2 q\2"
+    set vi-ins-mode-string "\1\e[6 q\2"
+  '';
 }
