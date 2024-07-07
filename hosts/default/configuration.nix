afa5f594a1b594b98cee6aad6ba4eda61d79d913
Sahil Dinanath <sahildinanath@gmail.com>
2024-07-07 19:10:18 +0200

    331 current  2024-07-07 19:10:06  24.11.20240703.9f4128e  6.9.7                           no-nvidia

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -243,7 +243,6 @@
 
   #######################################
   #developer settings
-  programs.bash.blesh.enable = true;
   programs.direnv.enable = true;
   documentation.dev.enable = true;
   ###################### i3 ############################################
