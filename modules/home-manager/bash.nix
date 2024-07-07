8a4e6249f56dcd0f2860ffc6b2908a6312d3c5af
Sahil Dinanath <sahildinanath@gmail.com>
2024-07-07 19:52:08 +0200

    335 current  2024-07-07 19:36:57  24.11.20240703.9f4128e  6.9.7                           no-nvidia

--- modules/home-manager/bash.nix
+++ modules/home-manager/bash.nix
@@ -39,6 +39,9 @@
 
       set -o vi
       bind '"jk":vi-movement-mode'
+
+    '';
+    interactiveShellInit = ''
       set show-mode-in-prompt on
       set vi-cmd-mode-string "\1\e[2 q\2"
       set vi-ins-mode-string "\1\e[6 q\2"
