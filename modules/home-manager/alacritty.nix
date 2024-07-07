7d024b204dd64ba3907a5400a977afd34c42c0ca
Sahil Dinanath <sahildinanath@gmail.com>
2024-07-07 15:19:54 +0200

    316 current  2024-07-07 15:19:44  24.11.20240703.9f4128e  6.9.7                           no-nvidia

---
+++ modules/home-manager/alacritty.nix
@@ -0,0 +1,17 @@
+{ pkgs, configs, ... }:
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.padding = {
+        x = 5;
+        y = 5;
+      };
+      font.normal = {
+        family = "FiraCode Nerd Font";
+        style = "Regular";
+      };
+      font.size = 12;
+    };
+  };
+}
